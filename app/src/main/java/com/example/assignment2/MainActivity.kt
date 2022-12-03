54393f2efd74c55c6c1a10c71ec1e2121bdb7744
Abd Alrahman Alahmar <abdalrahman.alahmar@student.griffith.ie>
2022-12-03 15:32:51 +0000

    Adding Reset Button Functionality
    Signed-off-by: Abd Alrahman Alahmar <abdalrahman.alahmar@student.griffith.ie>

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -10,38 +10,42 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity(),ArimaaDelegate {
 
-    private var arimaaGame: ArimaaGame? = null
+    private lateinit var arimaaGame: ArimaaGame
     private lateinit var finishTurnButton: Button
+    private lateinit var resetButton: Button
     private lateinit var currentPlayer: TextView
     private lateinit var error: TextView
-    private  var customView: CustomView? = null
+    private lateinit var customView: CustomView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         currentPlayer = findViewById<TextView>(R.id.currentPlayer)
+        resetButton = findViewById<Button>(R.id.reset_button)
         error = findViewById<TextView>(R.id.error)
         finishTurnButton = findViewById<Button>(R.id.finish_button)
-       customView = findViewById<CustomView>(R.id.custom_view)
+        customView = findViewById<CustomView>(R.id.custom_view)
+
 
         arimaaGame = ArimaaGame(this)
-        customView!!.Start(arimaaGame)
+        customView.Start(arimaaGame)
         endTurn(true)
 
-
-
         finishTurnButton.setOnClickListener{
-            arimaaGame!!.finishTurn()
+            arimaaGame.finishTurn()
+        }
+
+        resetButton.setOnClickListener{
+            arimaaGame = ArimaaGame(this)
+            customView.Start(arimaaGame)
+            endTurn(true)
         }
     }
 
+
     override fun endTurn(goldPlayer: Boolean) {
 
-        if (arimaaGame == null) {
-            currentPlayer.text = null
-            return
-        }
         val current = if (goldPlayer) "Gold" else "Silver"
         error.text = (null)
         currentPlayer.text = ("Current turn: $current")
@@ -53,7 +57,7 @@ class MainActivity : AppCompatActivity(),ArimaaDelegate {
 
 
     override fun updateBoard() {
-        customView!!.invalidate()
+        customView.invalidate()
     }
 
     override fun showMessage(message: String?) {
